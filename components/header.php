168968f5f5c9d322666ba87fd2feb6545f594ccd
kolya130899 <kolyawote75@gmail.com>
2021-03-19 02:28:48 +0200

    add faqs page

--- components/header.php
+++ components/header.php
@@ -7,14 +7,14 @@
   <nav class="header__navbar-mobile">
     <a href="#" class="text" id="open-mobile-subnavbar">代谢服务</a>
     <nav class="header__navbar-mobile--subnavbar">
-      <a href="<?= $url; ?>essay-writing" class="text">Essay 代写服务</a>
-      <a href="<?= $url; ?>online-course" class="text">网课代写服务</a>
-      <a href="<?= $url; ?>exams" class="text">考试代写服务</a>
-      <a href="<?= $url; ?>assignment" class="text"> Assignment 代写服务</a>
+      <a href="<?= $url; ?>essay-writing/" class="text">Essay 代写服务</a>
+      <a href="<?= $url; ?>online-course/" class="text">网课代写服务</a>
+      <a href="<?= $url; ?>exams/" class="text">考试代写服务</a>
+      <a href="<?= $url; ?>assignment/" class="text"> Assignment 代写服务</a>
     </nav>
     <a href="#" class="text">计算价格</a>
     <a href="#" class="text">博客</a>
-    <a href="#" class="text">常见问题</a>
+    <a href="<?= $url; ?>faq/" class="text">常见问题</a>
     <a href="#" class="text">联系客服</a>
   </nav>
 
@@ -26,14 +26,14 @@
   <nav class="header__navbar">
     <a href="#" class="text" id="open-subnavbar">代谢服务</a>
     <nav class="header__navbar--subnavbar">
-      <a href="<?= $url; ?>essay-writing" class="text">Essay 代写服务</a>
-      <a href="<?= $url; ?>online-course" class="text">网课代写服务</a>
-      <a href="<?= $url; ?>exams" class="text">考试代写服务</a>
-      <a href="<?= $url; ?>assignment" class="text"> Assignment 代写服务</a>
+      <a href="<?= $url; ?>essay-writing/" class="text">Essay 代写服务</a>
+      <a href="<?= $url; ?>online-course/" class="text">网课代写服务</a>
+      <a href="<?= $url; ?>exams/" class="text">考试代写服务</a>
+      <a href="<?= $url; ?>assignment/" class="text"> Assignment 代写服务</a>
     </nav>
     <a href="#" class="text js-header-link active-page">计算价格</a>
     <a href="#" class="text js-header-link ">博客</a>
-    <a href="#" class="text js-header-link ">常见问题</a>
+    <a href="<?= $url; ?>faq/" class="text js-header-link ">常见问题</a>
     <a href="#" class="text js-header-link ">联系客服</a>
   </nav>
   <!-- BUTTON -->
---
+++ faq/faq.js
@@ -0,0 +1,155 @@
+var faqs = [
+  {
+    tab: "订单价格与付款方式",
+    faqs: [
+      {
+        title: "你们的服务是如何定价的?",
+        text:
+          "<p>影响论文代写价格最基本的因素包括论文字数, 学习等级以及due date. Edu Expert Care采用一站自助算出价格的系统, 填写订单要求后系统会自动报价</p>",
+      },
+      {
+        title: "付款方式有哪几种?",
+        text:
+          "<p>我们支持PayPal, UnionPay; VISA Card; Master Card; Alipay; Bank Transfer 各种信用卡级银行转账和支付宝等付款方式。在支付方面遇到任何问题, 都可以咨询我们的客服人员</p>",
+      },
+      {
+        title: "可以打折吗? 有任何折扣吗?",
+        text:
+          "<p>Edu Expert Care的论文代写服务是不议价的, 不过我们会不定期通过email发送一些优惠code、你可以关注一下、成功邀请朋友下单的老客户, 可以得到折扣哦!</p>",
+      },
+    ],
+  },
+  {
+    tab: "账户安全",
+    faqs: [
+      {
+        title: "个人信息匿名保密吗? 不会泄露吧?",
+        text:
+          "<p>Edu Expert Care为了保护客户的个人信息和论文安全, 所有资料全部通过银行级SSL数据加密, 做到完全匿名保密! 我们和所有工作人员签订了保密协议, 绝不容许泄露客户隐私</p>",
+      },
+    ],
+  },
+  {
+    tab: "下单与交稿过程",
+    faqs: [
+      {
+        title: "完成订单最加急的时间是多少？",
+        text:
+          "<p>Edu Expert Care提供最快12小时加急服务. 我们有丰富的经验处理加急的订单. 不论您的deadline有多紧急, 我们都能在最短时间内完成高质量论文. 写手和客服团队24小时在线, 都有能力和经验处理您的紧急订单.<br><br>不过, 如果是特别紧急的订单, 如12小时之内的, 需要提前找客服确认一下, 我们确保有合适的老师才可以接单, 以免浪费您的时间</p>",
+      },
+      {
+        title: "我如何收到我的订单？",
+        text:
+          "<p>一旦您的订单被专家完成的，订单完成论文经过我们的审核和检测后，论文会随同Turnitin     查重报告发送到您的电子邮件地址</p>",
+      },
+      {
+        title: "确保准时交付吗?",
+        text:
+          "<p>我们确保在规定时间内将完成的论文交到您的手中. 准时交稿是一家代写公司应该做到的最基本, 我们会严格审核订单时间, 100％在约定时间前交付</p>",
+      },
+      {
+        title: "Essay代写下单流程是什么？",
+        text:
+          "<p><b>Step 1:</b> 方便注册快速下单 Facebook一键登录省时省力，三分钟即可完成下单，随时随地查 看订单状态<br><br> <b>Step 2:</b> 安全快捷支付 支持全球货币，定期推出优惠活动<br></br> <b>Step 3:</b> 完成填写订单要求, 准时交付 提交订单后，同专业写手开始写作，文章完成后发送至  你的邮箱，所有信息安全保密<br><br> <b>Step 4:</b> 14 天内免费无限修改 文章完成后14天内，无限次免费修改，100%满意保证</p>",
+      },
+    ],
+  },
+  {
+    tab: "写手的背景",
+    faqs: [
+      {
+        title: "你们能保证我的论文将被英语为母语的写手完成吗？",
+        text:
+          "<p>我们绝大部分的写手是美国、加拿大、英国、澳大利亚来的，他们以英语为母语。写手都有硕士或博士学历。我们按照你留学国家为你安排专业的写手</p>",
+      },
+      {
+        title: "你们如何保证写手的写作能力？",
+        text:
+          "<p>我们只聘请专业的写手与编辑团员。他们都必须通过复杂的申请流程，然后才能开始接任何任务。我们质量保证的团队检查和评估写手通过的学术考试。写手成功后，我们将检查个人信息，并允许首先处理几个模拟订单。质量保证团队会监控写手在特定时期内的工作，最终确定作家是否值得跟我们合作。每个月我们跟写手举行专业的培训<br><br> 所有EduExpertCare作家都需要完成以下资格才能被考虑：</p><ul><li>提供必要和准确的个人信</li><li>给我们介绍专业技能和写作经验</li><li>通过并完成30分钟的考试</li><li>提供一个Essay，显示出扎实的风格，结构和适当的英语知识</li><li>提供文件：提交文凭副本或高等教育证明以及身份证副本</li></ul><br><br><p>下单以后，我们大数据分配的系统, 会根据你的填写要求安排最好的专家</p>",
+      },
+      {
+        title: "我怎么跟写手联系？",
+        text:
+          "<p>正如我们的客户喜欢匿名保密，我们的写手同样如此。然而，我们理解，有时写手与客户的沟通是写作成功的关键，所以，您可以通过我们的网站平台向写手直接发送信息，与写手交流</p>",
+      },
+    ],
+  },
+  {
+    tab: "质量与原创性",
+    faqs: [
+      {
+        title: "如果我对论文不满意怎么办?",
+        text:
+          "<p>收到论文后, 如果您有任何不满意或者需要修改的地方, 可以联系客服或者直接在线与老师沟通; 我们提供14天内免费无限次修改, 直到您满意为止</p>",
+      },
+      {
+        title: "论文保证原创吗？文章有成绩保证吗？",
+        text:
+          "<p> EduExpert将客户的利益放在首位。所有文章我们都严格把控质量，文章100%准时交付，100%原创。收到文章后14天之内，提供无限次免费修改。平均成绩A或Ｂ（85+）， 成绩不满意就全额退款。所有的论文都经过Turnitin系统检测。订单完成后，客服会提供Turnitin检测报告</p>",
+      },
+      {
+        title: "质量如何保证？",
+        text:
+          "<p>专业团队进行监督并多次审核后才能发送给你看看论文，对于你的文章提供免费的Turnitin检测报告，请不用担心课程及作业的品质</p>",
+      },
+    ],
+  },
+  {
+    tab: "网课代写常见问题",
+    faqs: [
+      {
+        title: "EduExpert 网课代写支持的课程有哪些？",
+        text:
+          "<p><b>商科网课代修</b></p><p><i>商科类网课代修支持</i></p><ul> <li>Business Studies(商科)专业</li><li>Economics(经济学)专业</li><li>Accounting and Finance(会计和金融)专业</li><li>Hospitality, Leisure, Recreation and Tourism (酒店，休闲，娱乐和旅游)专业</li><li>Librarianship and Information Management (图书馆和信息管理)专业</li><li>Land and Property Management (土地及物业管理)专业</li></ul><p><b>工科网课代修</b></p><p><i>工科类网课代修支持但不限</i></p><ul> <li>Computer Science(计算机科学)专业</li><li>Mechanical Engineering(机械工程)专业</li><li>Electrical and Electronic Engineering (电器及电子工程)专业</li><li>General Engineering(一般工程)专业</li><li>Civil Engineering(土木工程)专业</li><li>Materials Technology(材料技术)专业</li><li>Aeronautical and Manufacturing Engineering (航空和制造工程学)专业</li><li>Architecture(建筑学)专业</li></ul><p><b>理科网课代修</b></p><p><i>理科类网课代修支持但不限</i></p><ul> <li>Mathematics(数学)专业</li><li>Medicine(医学)专业</li><li>Chemistry(化学)专业</li><li>Food Science(营养学)专业</li><li>Biological Sciences(生物科学)专业</li><li>Geology(地质学)专业</li><li>Nursing(护理)专业</li><li>Physics and Astronomy(物理和天文)专业</li></ul><p><b>文科网课代修</b></p><p><i>文科类网课代修支持但不限</i></p><ul> <li>Law(法律)专业</li><li>Art and Design(艺术)专业</li><li>History(历史)专业</li><li>Anthropology(人类学)专业</li><li>Music(音乐)专业</li><li>Linguistics(语言学)专业</li><li>English(英语)专业</li><li>Sociology(社会学)专业</li></ul>",
+      },
+    ],
+  },
+];
+
+// SWITCH FAQS TABS
+function switchFaqsTabs() {
+  $(".faq-nav-item").on("click", function () {
+    if (!$(this).hasClass("active")) {
+      $(".faq-nav-item.active").removeClass("active");
+      $(this).addClass("active");
+
+      var tabName = $(this).find("p").text();
+
+      printFaqs(tabName);
+    }
+  });
+}
+// TOGGLE FAQs
+function toggleFaq() {
+  $(document).on("click", ".faq-item", function () {
+    $(this).toggleClass("active");
+    $(this).find(".faq-item__body").slideToggle(300);
+  });
+}
+
+// PRINT FAQS ELEMENTS
+function printFaqs(tabName) {
+  faqs.forEach(function (item) {
+    if (item.tab === tabName) {
+      var faqElements = item.faqs.map(function (item) {
+        var element =
+          '<div class="faq-item"><h3>' +
+          item.title +
+          '</h3><div class="faq-item__body">' +
+          item.text +
+          "</div></div>";
+
+        return element;
+      });
+
+      var elements = faqElements.join("");
+      $(".faq-items__wrapper").html(elements);
+    }
+  });
+}
+
+$(document).ready(function () {
+  printFaqs(faqs[0].tab);
+  switchFaqsTabs();
+  toggleFaq();
+});
